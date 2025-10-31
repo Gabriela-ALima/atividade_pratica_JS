7f0b022372e65091e1992722e99a7c3e986a122f
Gabriela Lima <gabriela.deabreulima@gmail.com>
2025-10-31 09:30:45 -0300

    ajustes nos exercícios

---
+++ 01-variaveisOperacionais/ex4.js
@@ -0,0 +1,16 @@
+import readline from 'readline-sync';
+
+let num1;
+let num2;
+let num3;
+let num4;
+let dif;
+
+num1 = readline.questionFloat("Digite o primeiro numero: ");
+num2 = readline.questionFloat("Digite o segundo numero: ");
+num3 = readline.questionFloat("Digite o terceiro numero: ");
+num4 = readline.questionFloat("Digite o quarto numero: ");
+
+dif = (num1 * num2) - (num3 * num4);
+
+console.log(`O valor da diferença é ${dif}`);
